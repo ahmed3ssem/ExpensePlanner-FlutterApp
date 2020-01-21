1b47008bc3ad0436e9afc00477a1e10c6caa4659
a7med <a7med.assem@gmail.com>
2020-01-21 13:29:09 +0200

    Date Formating

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,11 +15,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transaction = [
-    Transaction(id: "t1",tittle: "New Mounth",amount: 3000.0,date: DateTime.now()),
-    Transaction(id: "t2",tittle: "Shoes",amount: 600.0,date: DateTime.now()),
+    Transaction(
+        id: "t1", tittle: "New Mounth", amount: 300.0, date: DateTime.now()),
+    Transaction(
+        id: "t2", tittle: "Shoes", amount: 600.0, date: DateTime.now()),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,33 +33,55 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             Container(
               width: double.infinity,
-            child:Card(
-              color: Colors.blue,
-              child: Text("Chart"),
-              elevation: 5,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Chart"),
+                elevation: 5,
+              ),
             ),
-            ),
-           Column(
-             children: transaction.map((tx){
-               return Card(
-                 child: Row(
-                   children: <Widget>[
-                     Container(
-                       margin:EdgeInsets.symmetric(vertical: 10 , horizontal: 15),
-                       decoration: BoxDecoration(border: Border.all(color: Colors.blue,width: 2)),
-                       padding: EdgeInsets.all(10),
-                       child: Text(tx.amount.toString()),),
-                     Column(
-                       children: <Widget>[
-                         Text(tx.tittle),
-                         Text(tx.date.toString())
-                       ],
-                     )
-                   ],
-                 ),
-               );
-             }).toList()
-             ),
+            Column(
+                children: transaction.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${tx.amount}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          color: Colors.purple
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            tx.tittle,
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                            DateFormat.yMMMd().format(tx.date),
+                            //DateFormat('yyyy-MM-dd').format(tx.date)
+                            style: TextStyle(
+                              color: Colors.grey
+                            ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList()),
           ],
         ));
   }
