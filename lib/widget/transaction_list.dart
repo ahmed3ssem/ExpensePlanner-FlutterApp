aee0f42566a9750d00956edfe3d9dd6194e33e5f
a7med <a7med.assem@gmail.com>
2020-01-22 12:48:53 +0200

    Adding Images to the App

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -10,7 +10,18 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 450,
-        child: ListView.builder(
+        child:userTransaction.isEmpty?
+            Column(
+              children: <Widget>[
+                Text("No transactions added yet",style: Theme.of(context).textTheme.title,),
+                SizedBox(height: 10,),
+                Container(
+                  height: 200,
+                 child:Image.asset("assets/images/waiting.png",fit: BoxFit.cover,),
+                )
+              ],
+            )
+        :ListView.builder(
             itemBuilder: (context,index){
                return Card(
                 child: Row(
