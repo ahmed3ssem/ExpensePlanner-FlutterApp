19c33503f254aa35f0fd64a7c944f5a3b2812dea
a7med <a7med.assem@gmail.com>
2020-01-22 11:11:37 +0200

    Improving & Styling the Model Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:expense_planner/model/transaction.dart';
 import 'package:expense_planner/widget/new_transaction.dart';
 import 'package:expense_planner/widget/transaction_list.dart';
-import 'package:expense_planner/widget/user_transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,15 +11,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget{
+  @override
+  MyHomePageState createState()=> MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
 
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  final List<Transaction> userTransaction = [
+    Transaction(id: "t1", tittle: "New Mounth", amount: 300.0, date: DateTime.now()),
+    Transaction(id: "t2", tittle: "Shoes", amount: 600.0, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String title , double amount)
+  {
+    final newTx = Transaction(id: DateTime.now().toString(),tittle: title,amount: amount,date: DateTime.now());
+
+    setState(() {
+      userTransaction.add(newTx);
+    });
+  }
+
+
+  void startAddNewTransaction(BuildContext context)
+  {
+    showModalBottomSheet(context:context, builder: (_){
+      //when click on any empty space , the add dialog will disapper
+      return GestureDetector(
+        onTap: (){},
+        child: NewTransaction(_addNewTransaction),
+        behavior: HitTestBehavior.opaque,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +60,14 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Expense Planner'),
           centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: (){})
+          ],
         ),
-        body: Column(
-          children: <Widget>[
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
             Container(
               width: double.infinity,
               child: Card(
@@ -38,8 +76,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransaction()
-          ],
-        ));
+            TransactionList(userTransaction)
+          ],),
+        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: ()=>startAddNewTransaction(context),
+      ),
+    );
   }
 }
