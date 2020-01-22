a9b3993d3747553a748de764af414be64d18d1bf
a7med <a7med.assem@gmail.com>
2020-01-22 16:34:32 +0200

    Adding a ListTile Widget

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -23,41 +23,21 @@ class TransactionList extends StatelessWidget {
             )
         :ListView.builder(
             itemBuilder: (context,index){
-               return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Theme.of(context).primaryColor, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${userTransaction[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).primaryColor),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          userTransaction[index].tittle,
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(userTransaction[index].date),
-                          //DateFormat('yyyy-MM-dd').format(tx.date)
-                          style: TextStyle(color: Colors.grey),
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8,horizontal: 5),
+                  child:ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child:Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                              child:Text('\$${userTransaction[index].amount}')),
                         )
-                      ],
-                    )
-                  ],
-                ),
+                    ),
+                    title: Text(userTransaction[index].tittle,style: Theme.of(context).textTheme.title,),
+                    subtitle: Text(DateFormat.yMMMd().format(userTransaction[index].date)),
+                  ),
               );
             },
             itemCount: userTransaction.length,
